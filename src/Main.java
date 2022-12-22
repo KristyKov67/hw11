ec7a6b6c328c2ed2b37b1ef1ebdc6546fe1c4618
Kristy <krisstyle@inbox.ru>
2022-12-22 23:29:21 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printGetYear(2024);
+
+
     }
-}
+    public static boolean getYear(int year) {
+        return (((year % 4 != 0) || (year % 400 != 0)) && (year % 100 == 0));
+    }
+
+    public static void printGetYear(int year) {
+        if (getYear(year)) {
+            System.out.println("Год высокосный");
+        } else {
+            System.out.println("Год невысокосный");
+        }
+
+
+
+    }}
